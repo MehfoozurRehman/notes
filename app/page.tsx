7b83d109e99bcd1397778a3d828323fc80762235
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-07-04 16:42:34 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,17 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 import { AddNote } from "./AddNote";
 import { Card } from "./Card";
 import { Note } from "@/prisma/interfaces";
+import prisma from "@/lib/prisma";
+import { unstable_noStore } from "next/cache";
 
-async function fetchData(cb?: (data: Note[]) => void) {
-  const response = await fetch("/api/notes", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
+export default async function page() {
+  unstable_noStore();
+
+  const notes = await prisma.note.findMany({
+    orderBy: {
+      id: "desc",
     },
   });
-
-  if (response.ok) {
-    const data = await response.json();
-    if (cb) {
-      cb(data);
-    }
-  } else {
-    console.error("Failed to fetch data");
-  }
-}
-
-export default function page() {
-  const [notes, setNotes] = useState<Note[]>([]);
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchData((data: Note[]) => {
-      setNotes(data);
-      setLoading(false);
-    });
-  }, []);
 
   return (
     <div
@@ -44,7 +21,8 @@ export default function page() {
         gap: "1rem",
       }}
     >
-      {loading ? (
+      <AddNote />
+      {notes?.length === 0 ? (
         <p
           style={{
             gridColumn: "1 / -1",
@@ -53,42 +31,10 @@ export default function page() {
             color: "gray",
           }}
         >
-          Loading...
+          No notes found
         </p>
       ) : (
-        <>
-          <AddNote
-            refetch={async () =>
-              await fetchData((data: Note[]) => {
-                setNotes(data);
-              })
-            }
-          />
-          {notes?.length === 0 ? (
-            <p
-              style={{
-                gridColumn: "1 / -1",
-                textAlign: "center",
-                fontSize: "1.5rem",
-                color: "gray",
-              }}
-            >
-              No notes found
-            </p>
-          ) : (
-            notes?.map((note) => (
-              <Card
-                key={note.id}
-                data={note}
-                refetch={async () =>
-                  await fetchData((data: Note[]) => {
-                    setNotes(data);
-                  })
-                }
-              />
-            ))
-          )}
-        </>
+        notes?.map((note: Note) => <Card key={note.id} data={note} />)
       )}
     </div>
   );
