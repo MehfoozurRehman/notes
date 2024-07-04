258c965bd13f56737bff99482ec4e491ba7f63d1
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-07-04 16:11:17 +0500

    Create AddNote.tsx

---
+++ app/AddNote.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+
+export function AddNote({ refetch }: { refetch: () => void }) {
+  const [adding, setAdding] = useState(false);
+
+  const addNote = async () => {
+    setAdding(true);
+    const prompt = window.prompt("Enter a title for the note", "New Note");
+
+    if (!prompt) {
+      return;
+    }
+
+    const response = await fetch("/api/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: prompt,
+        content: "",
+        color: "blue",
+      }),
+    });
+
+    if (response.ok) {
+      await response.json();
+
+      await refetch();
+    } else {
+      console.error("Failed to add note");
+    }
+    setAdding(false);
+  };
+  return (
+    <button
+      disabled={adding}
+      style={{
+        padding: "0.5rem",
+        fontSize: "1rem",
+        fontWeight: "bold",
+        color: "black",
+        backgroundColor: "lightgray",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+        minHeight: 200,
+        opacity: adding ? 0.5 : 1,
+      }}
+      onClick={addNote}
+    >
+      Add Note
+    </button>
+  );
+}
